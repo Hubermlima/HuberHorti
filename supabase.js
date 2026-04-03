2718d188c5cf4df2bc0805fbb180bbf358b06ec0
Huber martins lima <36750640+Hubermlima@users.noreply.github.com>
2026-04-03 14:51:11 -0400

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = 'https://suwaqkxkhhfopjkhsptf.supabase.co';
+
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1d2Fxa3hraGhmb3Bqa2hzcHRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4MDI2MDQsImV4cCI6MjA5MDM3ODYwNH0.HcLs3bkZZ0lATGSQbWtTt7oIxcM8inYmLZHm2K5v39U';
+
+window.db = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
